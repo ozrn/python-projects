0d9fb823d860b8fa92461742a0c630a5417805c2
unknown <ozerenkevser92@gmail.com>
2023-07-11 13:51:59 -0400

    Create variable to keep track of number of guesses

--- number_guessing_game.py
+++ number_guessing_game.py
@@ -14,8 +14,10 @@ else:
     quit()        
 
 random_number = random.randint(0, top_of_range)
+guesses = 0
 
 while True:
+    guesses += 1
     user_guess = input('Make a guess: ')
     if user_guess.isdigit:
         user_guess = int(user_guess)
@@ -27,4 +29,6 @@ while True:
         print('You got it!')
         break
     else:
-        print('You got it wrong!')    
+        print('You got it wrong!')  
+
+print('You got it in', guesses, 'guesses.' )          
