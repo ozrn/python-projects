ca4dc4bab721e1df7db9e9ba509eb3c894171ac2
unknown <ozerenkevser92@gmail.com>
2023-07-13 14:45:30 -0400

    Check if user input is in options or not

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -3,3 +3,13 @@ import random
 user_score = 0
 computer_score = 0
 
+options = ['rock', 'paper', 'scissors']
+
+while True:
+    user_input = input('Type Rock/Paper/Scissors or Q to quit: ').lower()
+    if user_input == 'q':
+        break
+
+    if user_input not in options:
+        continue   
+
