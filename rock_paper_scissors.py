59fd561facee524306d430320db3dd14da05a571
unknown <ozerenkevser92@gmail.com>
2023-07-13 15:12:22 -0400

    Use conditional statements to decide who wins

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -13,3 +13,28 @@ while True:
     if user_input not in options:
         continue   
 
+    random_num = random.randint(0, 2)
+    # rock: 0, paper: 1, scissors:2
+    computer_pick = options[random_num]
+    print('Computer picked', computer_pick + '.')
+
+    if user_input == 'rock' and computer_pick == 'scissors':
+        print('You won!')
+        user_score += 1
+
+    elif user_input == 'paper' and computer_pick == 'rock':
+        print('You won!')
+        user_score += 1
+
+    elif user_input == 'scissors' and computer_pick == 'paper':
+        print('You won!')
+        user_score += 1
+
+    else:
+        print('You lost!')
+        computer_score += 1  
+
+print('You won', user_score, 'times. ')
+print('Computer won', computer_score, 'times. ')
+print('Goodbye!')              
+
